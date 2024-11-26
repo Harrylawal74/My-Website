fc3ba1ecf8ad860cccfc4ea4c382ab7add9cdfc6
Harry Lawal <harrylawal74@gmail.com>
2024-11-26 13:07:03 +0000

    update navbutton.js
    clicking on any nav bar button closes the side nav bar

---
+++ navbutton.js
@@ -0,0 +1,6 @@
+function toggleCheckbox() {
+    const checkbox = document.querySelector('#sidebar-active');
+    // Do something with the input value, e.g., send it to a server
+    checkbox.checked = !checkbox.checked;
+
+}
---
+++ script.js
@@ -0,0 +1,5 @@
+function submitInput() {
+    const inputValue = document.getElementById('myInput').value;
+    // Do something with the input value, e.g., send it to a server
+    console.log(inputValue);
+  }
