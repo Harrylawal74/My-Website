d7fb047213476d01f8d8a62da566156971dd39b3
Harry Lawal <harrylawal74@gmail.com>
2024-12-09 01:53:45 +0000

    update

--- scripts/email.js
+++ scripts/email.js
@@ -1,16 +1,21 @@
-const myTimeout = setTimeout(clearData, 100);
+
+function myScript(){
+  const myTimeout = setTimeout(clearData, 100);
 
 
-function clearData() {
-  const name = document.getElementById('name');
-  const email = document.getElementById('email');
-  const number = document.getElementById('number');
-  const subject = document.getElementById('subject');
-  const body = document.getElementById('body');
-
-  name.value = '';
-  email.value = '';
-  number.value = '';
-  subject.value = '';
-  body.value = '';
+  function clearData() {
+    const name = document.getElementById('name');
+    const email = document.getElementById('email');
+    const number = document.getElementById('number');
+    const subject = document.getElementById('subject');
+    const body = document.getElementById('body');
+  
+    name.value = '';
+    email.value = '';
+    number.value = '';
+    subject.value = '';
+    body.value = '';
+  }
 }
+
+
